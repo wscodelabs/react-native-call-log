a032fb3c3ed03a8f8786b50c3d022902aca4a0c5
Bereket Abraham <babraham42@gmail.com>
2022-06-15 21:01:40 -0400

    call-logs: Fix CallType enum class

--- index.d.ts
+++ index.d.ts
@@ -1,35 +1,31 @@
-declare namespace CallLogs {
-  export enum callType {
-    OUTGOING = 'OUTGOING',
-    INCOMING = 'INCOMING',
-    MISSED = 'MISSED',
-    VOICEMAIL = 'VOICEMAIL',
-    REJECTED = 'REJECTED',
-    BLOCKED = 'BLOCKED',
-    ANSWERED_EXTERNALLY = 'ANSWERED_EXTERNALLY',
-    UNKNOWN = 'UNKNOWN',
-  }
+export type CallType =
+    'OUTGOING'
+  | 'INCOMING'
+  | 'MISSED'
+  | 'VOICEMAIL'
+  | 'REJECTED'
+  | 'BLOCKED'
+  | 'ANSWERED_EXTERNALLY'
+  | 'UNKNOWN';
 
-  export interface CallFilter {
-    minTimestamp?: number;
-    maxTimestamp?: number;
-    types?: CallType | CallType[];
-    phoneNumbers?: string | string[];
-  }
-
-  export interface CallLog {
-    phoneNumber: string;
-    duration: number;
-    name: string;
-    timestamp: string;
-    dateTime: string;
-    type: CallType;
-    rawType: number;
-  }
-
-  const load: (limit: number, filter?: CallFilter) => Promise<CallLog[]>;
-
-  const loadAll: () => Promise<CallLog[]>;
+export interface CallFilter {
+  minTimestamp?: number;
+  maxTimestamp?: number;
+  types?: CallType | CallType[];
+  phoneNumbers?: string | string[];
 }
 
-export = CallLogs;
+export interface CallLog {
+  phoneNumber: string;
+  duration: number;
+  name: string;
+  timestamp: string;
+  dateTime: string;
+  type: CallType;
+  rawType: number;
+}
+
+export function load(limit: number, filter?: CallFilter): Promise<CallLog[]>;
+
+export function loadAll(): Promise<CallLog[]>;
+
